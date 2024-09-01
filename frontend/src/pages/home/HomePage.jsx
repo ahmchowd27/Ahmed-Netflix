a435ceee8397e5742824f553aa452e3f6a8c7db4
Ahmed Chowdhury <ahmedssofa@gmail.com>
2024-09-01 03:45:35 -0400

    NAN

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,11 +1,10 @@
 import { useAuthStore } from "../../store/authUser";
-import AuthScreen from "./AuthScreen"; // This should match the actual file name.
-
+import AuthScreen from "./AuthScreen";
 import HomeScreen from "./HomeScreen";
 
 const HomePage = () => {
-  const { user } = useAuthStore();
+	const { user } = useAuthStore();
 
-  return <>{user ? <HomeScreen /> : <AuthScreen />}</>;
+	return <>{user ? <HomeScreen /> : <AuthScreen />}</>;
 };
 export default HomePage;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "./", // This makes sure all paths are relative
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+	plugins: [react()],
+	server: {
+		proxy: {
+			"/api": {
+				target: "http://localhost:5000",
+			},
+		},
+	},
 });
