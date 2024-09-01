6b1d21efc38146935b8bd737097ff134e9a83b16
Ahmed Chowdhury <ahmedssofa@gmail.com>
2024-09-01 00:53:16 -0400

    made couple change

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import cookieParser from "cookie-parser";
 import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
-import movieRoutes from "./routes/movie.route.js"; // Ensure it uses the updated version
+import movieRoutes from "./routes/movie.route.js";
 import tvRoutes from "./routes/tv.route.js";
 import searchRoutes from "./routes/search.route.js";
 
@@ -12,12 +12,12 @@ import { connectDB } from "./config/db.js";
 import { protectRoute } from "./middleware/protectRoute.js";
 
 const app = express();
+
 const PORT = ENV_VARS.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json());
+app.use(express.json()); // will allow us to parse req.body
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", movieRoutes);
@@ -25,7 +25,7 @@ app.use("/api/v1/tv", protectRoute, tvRoutes);
 app.use("/api/v1/search", protectRoute, searchRoutes);
 
 if (ENV_VARS.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend", "dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
@@ -33,12 +33,6 @@ if (ENV_VARS.NODE_ENV === "production") {
 }
 
 app.listen(PORT, () => {
-  console.log("Server is running on port " + PORT);
+  console.log("Server started at http://localhost:" + PORT);
   connectDB();
-});
-
-// Example error-handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something broke!");
 });
