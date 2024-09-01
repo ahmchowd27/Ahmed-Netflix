dfc42fc19b76b090820a29ad7ab8c3dadf6b7048
Ahmed Chowdhury <ahmedssofa@gmail.com>
2024-09-01 03:46:12 -0400

    NOPE

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,10 +1,11 @@
 import { useAuthStore } from "../../store/authUser";
-import AuthScreen from "./AuthScreen";
+import AuthScreen from "./AuthScreenComponent"; // This should match the actual file name.
+
 import HomeScreen from "./HomeScreen";
 
 const HomePage = () => {
-	const { user } = useAuthStore();
+  const { user } = useAuthStore();
 
-	return <>{user ? <HomeScreen /> : <AuthScreen />}</>;
+  return <>{user ? <HomeScreen /> : <AuthScreen />}</>;
 };
 export default HomePage;
